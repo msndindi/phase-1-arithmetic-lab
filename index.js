53d833e2d4a3b32a32a0ab1c5e8a55720d9b62c1
Mutindi Hope <msndindi@outlook.com>
2023-04-05 13:26:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = 3;
+Number.isInteger(random);
+
+
+let num3 = 9;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let max = Math.max(19, 13, 20);
+
